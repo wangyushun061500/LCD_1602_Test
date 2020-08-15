ebd4b72e43a61916e2204afb04ea7d82ae43b719
YuShun <2305808176@qq.com>
2020-08-15 21:02:16 +0800

    完成引脚定义

--- config.h
+++ config.h
@@ -3,4 +3,22 @@
 
 #include "15w.h"
 
+/*引脚定义**/
+#define VSS P17
+#define GND P16 
+#define VO P15
+#define RS P14
+#define RW P13
+#define CE P12
+#define D0 P11
+#define D1 P10
+#define D2 P07
+#define D3 P06
+#define D4 P05
+#define D5 P04
+#define D6 P03
+#define D7 P02
+#define LEDA P01
+#define LEDK P00
+
 #endif
