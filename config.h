cc3084ecdfc6baa455c941b78b03e78ba309f79a
YuShun <2305808176@qq.com>
2020-08-15 21:59:22 +0800

    加入基本的读写操作函数

--- config.h
+++ config.h
@@ -2,6 +2,12 @@
 #define __CONFG_H_
 
 #include "15w.h"
+#include "INTRINS.H"
+
+/***数据类型定义（u表示数据，数字表示数据的长度）***/
+typedef 	unsigned char	u8;
+typedef 	unsigned int	u16;
+typedef 	unsigned long	u32;
 
 /*引脚定义**/
 #define VSS P17
--- mian.c
+++ mian.c
@@ -1,7 +1,13 @@
 #include "config.h"
 
+
 //函数提前声明
 void init (void);
+void Delay100us();
+u8 LCD_Data_Read (void);
+void LCD_Data_Write (u8 Data);
+void Write_Cmd(u8 cmd);
+void Write_Dat(u8 dat);
 
 void main (void)
 {
@@ -24,4 +30,65 @@ void init (void)
     GND = 0;
     LEDA = 1;
     LEDK = 0;
+}
+
+//延时函数100us
+void Delay100us()		//@24.000MHz
+{
+	unsigned char i, j;
+
+	i = 3;
+	j = 82;
+	do
+	{
+		while (--j);
+	} while (--i);
+}
+
+//读数据
+u8 LCD_Data_Read (void)
+{
+     u8 Data;
+     Data = (P0 >> 2) | (P1 << 6);
+     Data = (Data << 4) | (Data >> 4);
+     Data = ((Data >> 2) & 0x33) | ((Data << 2) & 0xcc);
+     Data = ((Data >> 1) & 0x55) | ((Data << 1) & 0xaa);
+     return Data;
+}
+
+//写数据
+void LCD_Data_Write (u8 Data)
+{
+    D0 = (bit)(Data & 0x01);
+    D1 = (bit)(Data & 0x02);
+    D2 = (bit)(Data & 0x04);
+    D3 = (bit)(Data & 0x08);
+    D4 = (bit)(Data & 0x10);
+    D5 = (bit)(Data & 0x12);
+    D6 = (bit)(Data & 0x14);
+    D7 = (bit)(Data & 0x18);
+}
+
+//写命令操作
+void Write_Cmd(u8 cmd)
+{
+	RS = 0;		                	//写入命令
+	RW = 0;
+	LCD_Data_Write(cmd);			
+    Delay100us();
+	CE = 1;		                	//使能端高电平
+	Delay100us();
+	CE = 0;
+}
+ 
+//写数据操作
+void Write_Dat(u8 dat)
+{
+	RS = 1;
+	RW = 0;
+	LCD_Data_Write(dat);
+	Delay100us();
+	CE = 1;			            //使能端高电平
+	Delay100us();
+	CE = 0;
 }
